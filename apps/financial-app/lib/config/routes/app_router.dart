432c1e7495069b83351b04d0281f48b63e6d0c93
Elkis Andres Rovira <rovira.05@hotmail.com>
2026-03-13 20:07:50 -0500

    feat: agregar soporte para acciones personalizadas en la barra de navegación

--- apps/financial-app/lib/config/routes/app_router.dart
+++ apps/financial-app/lib/config/routes/app_router.dart
@@ -1,5 +1,7 @@
 import 'package:bot_toast/bot_toast.dart';
+import 'package:components/language_switcher.dart';
 import 'package:components/layout/scaffold_with_navigation.dart';
+import 'package:components/theme_button.dart';
 import 'package:core/enum/navigation_item.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:feature_funds/presentation/funds/view/funds_view.dart';
@@ -7,6 +9,7 @@ import 'package:feature_funds/routes.dart';
 import 'package:flutter/foundation.dart';
 
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:injectable/injectable.dart';
 
@@ -27,6 +30,11 @@ GoRouter _createRouter() {
       StatefulShellRoute.indexedStack(
         builder: (context, state, navigationShell) {
           return ScaffoldWithNavigation(
+            appBarActions: [
+              const ThemeModeButton.icon(),
+              const LanguageSwitcherButton(),
+              const Gap(2),
+            ],
             logoPath: 'assets/img/logo.png',
             logoDarkPath: 'assets/img/logo_dark.png',
             key: ValueKey(context.locale.toString()),
