46f6630ae32aeea299c0abcf4fc29b4472301c6d
Elkis Andres Rovira <rovira.05@hotmail.com>
2026-05-06 00:11:38 +0000

    refactor: limpiar comentarios innecesarios en los archivos injectable de Home y Wishlist

--- packages/client-features/home/lib/injectable.dart
+++ packages/client-features/home/lib/injectable.dart
@@ -1,7 +1,4 @@
 import 'package:injectable/injectable.dart';
 
-/// Marca este package como [MicroPackage].
-/// [injectable_config_builder] generará un [MicroPackageModule] que el app raíz
-/// incluye automáticamente en su método `init()` sin llamadas manuales.
 @InjectableInit.microPackage()
-void initMicroPackage() {} // stub requerido por el generador, nunca se llama
+void initMicroPackage() {}
