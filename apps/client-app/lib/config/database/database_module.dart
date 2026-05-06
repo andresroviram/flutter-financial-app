0111d49f69a73b136f0fde551e0913398df81d46
Elkis Andres Rovira <rovira.05@hotmail.com>
2026-05-06 00:11:38 +0000

    refactor: remove wishlist feature implementation
    - Deleted the entire wishlist feature including repository, use cases, bloc, events, states, and UI components.
    - Cleaned up related routes and dependencies in pubspec.yaml and other configuration files.

--- apps/client-app/lib/config/database/database_module.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter_wigilabs_sr/config/database/app_database.dart';
-import 'package:injectable/injectable.dart';
-
-@module
-abstract class DatabaseModule {
-  @singleton
-  AppDatabase get database => AppDatabase();
-}
